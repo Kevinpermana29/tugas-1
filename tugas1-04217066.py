02a7a6d44593241a75cd1318353857553620b351
Kevinpermana29 <32183656+Kevinpermana29@users.noreply.github.com>
2017-09-22 09:33:00 +0700

    Add files via upload

---
+++ tugas1-04217066.py
@@ -0,0 +1,9 @@
+nama = "kevin permana"
+nim = "04217066"
+umur = 19
+print ("namaku:" + nama)
+print ("nimku :"+ nim)
+print ( "umur" )
+print (umur)
+print ("umur 5 tahun kedepan")
+print (umur +5 )
